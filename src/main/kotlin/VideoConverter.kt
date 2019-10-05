95936a3cf9b18c9674622cbc34319746534468a7
AndreyBychkov <abychkov@edu.hse.ru>
2019-10-06 01:06:24 +0300

    Added video converting feature

---
+++ src/main/kotlin/VideoConverter.kt
@@ -0,0 +1,23 @@
+import ws.schild.jave.*
+import java.io.File
+
+class VideoConverter {
+    fun convert(source: File, target: File) {
+        val audio = AudioAttributes().apply {
+            setCodec(AudioAttributes.DIRECT_STREAM_COPY)
+        }
+
+        val video = VideoAttributes().apply {
+            setCodec("mpeg4")
+        }
+
+        val encodingAttributes = EncodingAttributes().apply {
+            format = target.extension
+            audioAttributes = audio
+            videoAttributes = video
+        }
+
+        val encoder = Encoder()
+        encoder.encode(MultimediaObject(source), target, encodingAttributes)
+    }
+}
---
+++ src/main/kotlin/VideoFormats.kt
@@ -0,0 +1,7 @@
+enum class VideoFormats {
+    AVI { override val extension = "avi" },
+    MP4 { override val extension = "mp4"},
+    MOV { override val extension = "mov"};
+
+    abstract val extension: String
+}
